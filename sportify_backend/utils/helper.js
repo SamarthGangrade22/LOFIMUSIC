3909ffae9cd981c64ec6aac99c7e76abdd7803dd
SamarthGangrade22 <samarthgangrade@gmail.com>
2024-05-16 10:25:35 +0530

    changes

--- sportify_backend/utils/helper.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-exports.getToken = async (email, user) => {
-    const { _id } = user; // Extract _id from the user object
-    const token = jwt.sign({ email, _id }, 'secret'); // Pass email and _id to jwt.sign
-    return token;
-};
-
-module.exports = exports;
